c65f443de0a487d1a3b8a91ccba4e40bc813a920
Simko Matej <sim0323@vsb.cz>
2022-10-03 15:41:58 +0200

    KS_hw_v2

--- qnaire/api/tests.py
+++ qnaire/api/tests.py
@@ -9,7 +9,10 @@ from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import re
+
 
 from .models import Questionnaire
 
@@ -19,20 +22,21 @@ class QuestionnaireUITest(TestCase):
     BASE_URL= "http://127.0.0.1:8000/"
 
     def setUp(self):
-        self.driver = webdriver.Firefox(executable_path="C:/Users/28D/Downloads/geckodriver.exe")
+        self.driver = webdriver.Firefox(executable_path="C:/Users/sim0323/Downloads/geckodriver.exe")
 
     def login(self):
         self.driver.get(f"{self.BASE_URL}login/")
         username_elem = self.driver.find_element(By.NAME, 'username')
         username_elem.send_keys('admin')
         admin_elem = self.driver.find_element(By.NAME, 'password')
+        admin_elem.clear()
         admin_elem.send_keys('EaSo8OK1aioMdH')
         self.driver.find_element(By.XPATH, '//form//button[@type="submit"]').click()
+        WebDriverWait(self.driver, 2).until(EC.presence_of_element_located((By.XPATH, '//h1')))
 
     def create_qnaire(self, name):
         self.driver.get(f"{self.BASE_URL}questionnaires/")
-        sleep(0.2) #the left sidebar sometimes takes too much time to show the button, so I sleep for a bit here
-        self.driver.find_element(By.XPATH, '//span[text()="Vytvořit dotazník"]').click()
+        WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Vytvořit dotazník"]'))).click()
         name_elem = self.driver.find_element(By.NAME, 'name')
         name_elem.send_keys(name)
         self.driver.find_element(By.XPATH, '//form//button[@type="submit"]').click()
@@ -56,17 +60,21 @@ class QuestionnaireUITest(TestCase):
         ('', False),
         (' ', True),
         ('x' * Questionnaire._meta.get_field('name').max_length, True),
-        ('x' * (Questionnaire._meta.get_field('name').max_length + 1), False)]
+        ('x' * (Questionnaire._meta.get_field('name').max_length + 1), False),
+        ('3ččš', True)]
 
     def test(self):
         self.login()
         for name, result in self.test_params:
             with self.subTest(f'name="{name}", expected_result:{result}'):
                 qnaire_id = self.create_qnaire(name)
+                self.assertEqual(qnaire_id != None, result)
+                if result:
+                    name_on_page = self.driver.find_element(By.ID, 'questionnaire-name').get_attribute('value')
+                    self.assertEqual(name_on_page, name)
                 if qnaire_id is not None:
                     self.delete_qnaire(qnaire_id)
-                self.assertEqual(qnaire_id != None, result)
-                #sleep(1)
+
 
     def test_unique_name(self):
         self.login()
@@ -75,7 +83,6 @@ class QuestionnaireUITest(TestCase):
 
         self.assertEqual(self.driver.current_url, f"{self.BASE_URL}questionnaires/")
         self.delete_qnaire(qnaire_id)
-        #sleep(2)
 
      
         
